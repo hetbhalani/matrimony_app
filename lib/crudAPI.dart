7c15920166d081a1f50481d02f8f14f1b19c1d85
hetbhalani <bhalanihet2006@gmail.com>
2025-03-18 23:09:09 +0530

    api done

--- lib/crudAPI.dart
+++ lib/crudAPI.dart
@@ -30,32 +30,42 @@ class API_Users{
     return List<Map<String, dynamic>>.from(jsonDecode(response.body));
   }
 
-  Future<void> updateUser(String id, String title, String description)async{
-    final body = {
-      "title": title,
-      "description": description,
-      "isDone": false
-    };
+  Future<void> updateUser(Map<String, dynamic> user,String id)async{
+
     final url = "https://67c70421c19eb8753e7845fe.mockapi.io/Matrimony_app/$id";
     final uri = Uri.parse(url);
     final response = await http.put(uri,
-        body: jsonEncode(body),
+        body: jsonEncode(user),
         headers: {"Content-Type": "application/json"}
     );
     print(response.body);
     print("Updated");
   }
 
-  Future<void> deleteUser(String id) async{
-    final url = "https://67c70421c19eb8753e7845fe.mockapi.io/todo/$id";
-    final uri = Uri.parse(url);
-    final response = await http.delete(
-        uri,
-        headers: {"Content-Type": "application/json"}
-    );
-
-    if(response.statusCode == 201){
-      print("Deleted successfully");
+  Future<void> deleteUser (String id) async{
+    final uri = Uri.parse('https://67c70421c19eb8753e7845fe.mockapi.io/Matrimony_app/$id');
+    try{
+      final response = await http.delete(uri);
+      if (response.statusCode == 200) {
+        print("User deleted successfully!");
+      } else {
+        print("Failed to delete user: ${response.statusCode}");
+      }
+    }catch(e){
+      print(e);
     }
+  }
+
+  Future<List<Map<String, dynamic>>> getFavouriteUsers() async {
+    List<Map<String, dynamic>> allUsers = await fetchUser();
+    
+    List<Map<String, dynamic>> favUsers = allUsers.where((user) {
+      int fav = user['isFav'] is int
+          ? user['isFav']
+          : int.parse(user['isFav'].toString()) ?? 0;
+      return fav == 1;
+    }).toList();
+    
+    return favUsers;
   }
 }
